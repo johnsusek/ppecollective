8ac69e37dca14e2cc0aaae0058343811060e1f88
John Susek <john@johnsolo.net>
2020-03-22 21:05:59 -0500

    SSL challenge

--- pages/index.vue
+++ pages/index.vue
@@ -1,40 +1,18 @@
 <template>
   <div class="container">
     <div>
-      <logo />
       <h1 class="title">
-        ppecollective
+        Chicago PPE Collective
       </h1>
       <h2 class="subtitle">
-        We got this.
+        Connecting makers with medicial facilities.
       </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
     </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
 export default {
-  components: {
-    Logo
-  }
 }
 </script>
 
